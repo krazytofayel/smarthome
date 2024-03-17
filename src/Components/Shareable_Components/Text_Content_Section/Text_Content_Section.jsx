555283c6969105bab1b35294db35f576bf68810d
krazytofayel <tofayel@krazyit.com>
2024-03-17 12:58:44 +0600

    (feat):there are 3 text formate header,title,content all are reuseable seperate component

---
+++ src/Components/Shareable_Components/Text_Content_Section/Text_Content_Section.jsx
@@ -0,0 +1,14 @@
+
+
+const Text_Content_Section = ({text_content}) => {
+  return (
+ <>
+ <div className="text-md text-black">
+  <p>{text_content}</p>
+ </div>
+ 
+ </>
+  )
+}
+
+export default Text_Content_Section
