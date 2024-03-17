9f8bb3860721700e34991325da23a1614a8bca37
krazytofayel <tofayel@krazyit.com>
2024-03-17 12:47:06 +0600

    import all home component

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from "react";
+import NavBar from "../../Components/Shareable_Components/NavBar/NavBar";
+import Home_Main_Section from "./Home_Components/Home_Main_Section/Home_Main_Section";
+import AboutUs_Section from "./Home_Components/AboutUs_Section/AboutUs_Section";
+
+
+const Home = () => {
+  const [navfix, setNavfix] = useState(false);
+
+  function setFixed() {
+    if (window.scrollY >= 70) {
+      setNavfix(true);
+      //console.log(scrollY)
+    } else {
+      setNavfix(false);
+    }
+  }
+  window.addEventListener("scroll", setFixed);
+  return (
+    <>
+      <div
+        className={`z-20 fixed top-0 bg-white w-full transition-all duration-300 ease-in-out ${
+          navfix ? "shadow-lg bg-white" : ""
+        }`}
+      >
+        <NavBar />
+      </div>
+     <Home_Main_Section/>
+     <AboutUs_Section/>
+    </>
+  );
+};
+
+export default Home;
