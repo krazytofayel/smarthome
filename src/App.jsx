2fdf240536fbce2c5120a17223ecca4e54678034
krazytofayel <tofayel@krazyit.com>
2024-03-17 14:05:32 +0600

    basic set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 function App() {
   
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
      
     </>
   )
