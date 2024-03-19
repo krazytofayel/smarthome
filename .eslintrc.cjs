88ec6df1b91fd5804aa08f2530090380af86a85d
krazytofayel <tofayel@krazyit.com>
2024-03-19 15:22:49 +0600

    *important changes for 's error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,21 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "react/no-unescaped-entities": [
+      "error",
+      {
+        forbid: [
+          {
+            char: ">",
+            alternatives: ["&gt;"],
+          },
+          {
+            char: "}",
+            alternatives: ["&#125;"],
+          },
+        ],
+      },
+    ],
     "react/prop-types": "off",
   },
 };
