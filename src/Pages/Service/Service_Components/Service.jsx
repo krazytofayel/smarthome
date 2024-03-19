b6e7bfd5629ee48ebd8d99c98f31bbc4d05a7266
krazytofayel <tofayel@krazyit.com>
2024-03-19 15:21:29 +0600

    (feat):service page divided multiple component

---
+++ src/Pages/Service/Service_Components/Service.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import NavBar from "../../../Components/Shareable_Components/NavBar/NavBar";
+import Service_Section from "./Service_Section/Service_Section";
+
+const Service = () => {
+  const [navfix, setNavfix] = useState(false);
+
+  function setFixed() {
+    if (window.scrollY >= 70) {
+      setNavfix(true);
+      //console.log(scrollY)
+    } else {
+      setNavfix(false);
+    }
+  }
+  window.addEventListener("scroll", setFixed);
+  return (
+    <>
+      <div
+        className={`z-20 fixed top-0 bg-white w-full transition-all duration-300 ease-in-out ${
+          navfix ? "shadow-lg bg-white" : ""
+        }`}
+      >
+        <NavBar />
+      </div>
+      <div className="mt-20">
+        <div className="bg-cover bg-center bg-no-repeat bg-hero-pattern  bg-blend-multiply h-[30rem]  mb-5">
+          <div className="flex flex-col justify-start ">
+            <h2 className="text-4xl font-bold text-white flex text-start mt-32 ml-14">
+              About Us
+            </h2>
+            <nav className="flex  ml-14" aria-label="Breadcrumb">
+              <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li className="inline-flex items-center">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-sm font-medium text-white hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                  >
+                    <svg
+                      className="w-3 h-3 me-2.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                    </svg>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <svg
+                      className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 6 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 9 4-4-4-4"
+                      />
+                    </svg>
+                    <a
+                      href="#"
+                      className="ms-1 text-sm font-medium text-white hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+                    >
+                      Projects
+                    </a>
+                  </div>
+                </li>
+                <li aria-current="page">
+                  <div className="flex items-center">
+                    <svg
+                      className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 6 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 9 4-4-4-4"
+                      />
+                    </svg>
+                    <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                      About us
+                    </span>
+                  </div>
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+        <Service_Section />
+      </div>
+    </>
+  );
+};
+
+export default Service;
