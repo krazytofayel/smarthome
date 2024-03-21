4757015dab97ad906418efc73198ce1e49be9976
krazytofayel <tofayel@krazyit.com>
2024-03-21 16:06:31 +0600

    service page component

--- src/Pages/Service/Service_Components/Service.jsx
+++ src/Pages/Service/Service_Components/Service.jsx
@@ -3,6 +3,8 @@ import NavBar from "../../../Components/Shareable_Components/NavBar/NavBar";
 import Service_Section from "./Service_Section/Service_Section";
 import FAQ_Section from "../../../Components/Shareable_Components/FAQ_Section/FAQ_Section";
 import Footer from "../../../Components/Shareable_Components/Footer/Footer";
+import Breadcrumb_Section from "../../../Components/Shareable_Components/Breadcrumb_Section/Breadcrumb_Section";
+import DynamicHeading from "../../../Components/Shareable_Components/Breadcrumb_Section/Breadcrumb_Components/DynamicHeading";
 
 const Service = () => {
   const [navfix, setNavfix] = useState(false);
@@ -16,6 +18,23 @@ const Service = () => {
     }
   }
   window.addEventListener("scroll", setFixed);
+   const breadcrumbItems = [
+     { text: "Home", href: "#", icon: "home" },
+     {
+       text: "Service",
+       href: "#",
+       linkClasses:
+         "text-sm font-medium text-white hover:text-blue-600 md:ms-2 ",
+       icon: "arrow",
+     },
+     {
+       text: "All Service",
+       href: "#",
+       linkClasses:
+         "text-sm font-medium text-white hover:text-blue-600 md:ms-2 ",
+       icon: "arrow",
+     },
+   ];
   return (
     <>
       <div
@@ -27,83 +46,16 @@ const Service = () => {
       </div>
       <div className="mt-20">
         <div className="bg-cover bg-center bg-no-repeat bg-hero-pattern  bg-blend-multiply h-[25rem]  mb-5">
-          <div className="flex flex-col justify-start ">
-            <h2 className="text-6xl font-bold text-white flex text-start mt-32 mb-4 ml-14">
-              About Us
-            </h2>
-            <nav className="flex  ml-14" aria-label="Breadcrumb">
-              <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                <li className="inline-flex items-center">
-                  <a
-                    href="#"
-                    className="inline-flex items-center text-sm font-medium text-white hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-                  >
-                    <svg
-                      className="w-3 h-3 me-2.5"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                    </svg>
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <div className="flex items-center">
-                    <svg
-                      className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 6 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="m1 9 4-4-4-4"
-                      />
-                    </svg>
-                    <a
-                      href="#"
-                      className="ms-1 text-sm font-medium text-white hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
-                    >
-                      Service
-                    </a>
-                  </div>
-                </li>
-                <li aria-current="page">
-                  <div className="flex items-center">
-                    <svg
-                      className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 6 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="m1 9 4-4-4-4"
-                      />
-                    </svg>
-                    <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
-                   all service
-                    </span>
-                  </div>
-                </li>
-              </ol>
-            </nav>
+          <div className="container mx-auto ">
+            <div className=" flex flex-col justify-start ">
+              <DynamicHeading />
+              <Breadcrumb_Section items={breadcrumbItems} />
+            </div>
           </div>
         </div>
         <Service_Section />
-        <FAQ_Section/>
-        <Footer/>
+        <FAQ_Section />
+        <Footer />
       </div>
     </>
   );
