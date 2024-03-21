c5a6ae82c63ba2a9966559752548829be52210be
krazytofayel <tofayel@krazyit.com>
2024-03-21 16:03:54 +0600

    add background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
       backgroundImage: {
         "hero-pattern":
           "url('/public/images/Service_Page_Images/service_page_images.png')",
+        "contact-us-img":
+          "url('/public/images/ContactUs_Page_Images/contactus.png')",
+        "blog-popular-post-img-1":
+          "url('/public/images/blog_Page_Images/Popular_Post_Images/popularpost_img-1.png')",
+        "blog-popular-post-img-2":
+          "url('/public/images/blog_Page_Images/Popular_Post_Images/popularpost_img-2.png')",
         "card-img1": "url('./src/assets/images/homepage/Maskgroup2.png')",
         "carve-img":
           "url('./src/assets/images/homepage/photo-handsome-male-student-his-female-groupmate-demonstrates-okay-gesture-agree-with-something_1-removebg-preview 1.png')",
