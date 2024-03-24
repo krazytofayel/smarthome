60461e2bf14ac0268b90c803729fa171268c62c0
krazytofayel <tofayel@krazyit.com>
2024-03-24 16:07:53 +0600

    here import route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Service from "./Pages/Service/Service_Components/Service";
+import Service from "./Pages/Service/Service";
 import Privacy_policy from "./Pages/Static_Page/Privacy_Policy/Privacy_policy";
 import Terms_Condition from "./Pages/Static_Page/Terms_Condition/Terms_Condition";
 import ContactUs from "./Pages/ContactUs/ContactUs";
 import Blog from "./Pages/Blog/Blog";
+import Blog_Details from "./Pages/Blog_Details/Blog_Details";
+import Service_Details from './Pages/Service/Service_Details/Service_Details'
 function App() {
   
 
@@ -14,10 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service />} />
+        <Route path="/service_details/:postId" element={<Service_Details />} />
         <Route path="/privacy_policy" element={<Privacy_policy />} />
         <Route path="/terms_and_condition" element={<Terms_Condition />} />
         <Route path="/contact_us" element={<ContactUs />} />
-        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:postId" element={<Blog_Details />} />
       </Routes>
     </>
   );
