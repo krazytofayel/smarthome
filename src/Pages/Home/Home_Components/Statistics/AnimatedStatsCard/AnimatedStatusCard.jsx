0e6a27041157522ce75c1fab70e46a1c7180cb17
krazytofayel <tofayel@krazyit.com>
2024-05-15 10:48:39 +0600

    home section -statics card changes

--- src/Pages/Home/Home_Components/Statistics/AnimatedStatsCard/AnimatedStatusCard.jsx
+++ src/Pages/Home/Home_Components/Statistics/AnimatedStatsCard/AnimatedStatusCard.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const AnimatedStatsCard = ({ title, value, icon }) => {
   const [count, setCount] = useState(0);
@@ -17,12 +17,12 @@ const AnimatedStatsCard = ({ title, value, icon }) => {
     <div className=" w-full bg-white shadow-lg rounded-lg overflow-hidden">
       <div className="p-4">
         <div className="flex lg:flex-col xl:flex-row justify-center items-center">
-          <div className="p-3 bg-[#00ABE4] rounded-full shadow-lg">{icon}</div>
-          <div className="ml-4">
+          {/* <div className="p-3 bg-[#00ABE4] rounded-full shadow-lg">{icon}</div> */}
+          <div>
             <p className="text-gray-600 text-3xl text-center font-bold">
               {count} +{" "}
             </p>
-            <h2 className="text-md font-semibold text-gray-800">{title}</h2>
+            <h2 className="text-md font-bold text-gray-800">{title}</h2>
           </div>
         </div>
       </div>
